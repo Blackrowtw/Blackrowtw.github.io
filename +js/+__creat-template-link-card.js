0f2d0eecfdb0490770a5cd7ade849d24417a1503
Blackrowtw <blackrow.020@gmail.com.tw>
2024-06-03 16:50:16 +0800

    Hexo Deploy 自動部屬 : 2024年6月3日 16:50:16

---
+++ +js/+__creat-template-link-card.js
@@ -0,0 +1,37 @@
+import { link_card_info } from "./+__link-card-info.js";
+// 定義生成 HTML 模板的函數
+function creatTemplate_link_card(link_data) {
+  return `
+    <div class="bk-link-card-scrollor">
+      <div class="bk-link-card">
+        <div class="bk-link-card-icon">
+          <img
+            src="${link_data.img_link}"
+            alt="icon-${link_data.title}.png is BROKEN!! :("
+            width="auto"
+            height="auto"
+          />
+        </div>
+        <div class="bk-link-card-body">
+          <a href="${link_data.url}" class="bk-link-card-title headerlink"
+            >${link_data.title}</a
+          >
+          <div class="bk-link-card-subtitle">
+            <div>${link_data.subtitle}</div>
+          </div>
+        </div>
+      </div>
+      <div class="bk-link-card-bg" id="bk-link-card-bg-${link_data.number}" style="background-color: ${link_data.color_rgb};">
+        <div class="bk-link-card-bg-inner"></div>
+      </div>
+    </div>
+  `;
+}
+
+// 在 HTML 中動態生成模板並填入資料
+const link_card_template_Container = document.getElementById(
+  "bk-link-card-template"
+);
+link_card_info.forEach((link_data) => {
+  link_card_template_Container.innerHTML += creatTemplate_link_card(link_data);
+});
--- +js/+__link-card-info.js
+++ +js/+__link-card-info.js
@@ -1,5 +1,5 @@
-// Card資料
-const link_card_info = [
+// 導出常數 Card資料
+export const link_card_info = [
   {
     number: "01",
     title: "Twitter",
@@ -129,40 +129,3 @@ const link_card_info = [
   //   img_link: "/+images/icon/icon-discord_x512.png",
   // },
 ];
-
-// 定義生成 HTML 模板的函數
-function creatTemplate_link_card(link_data) {
-  return `
-    <div class="bk-link-card-scrollor">
-      <div class="bk-link-card">
-        <div class="bk-link-card-icon">
-          <img
-            src="${link_data.img_link}"
-            alt="icon-${link_data.title}.png is BROKEN!! :("
-            width="auto"
-            height="auto"
-          />
-        </div>
-        <div class="bk-link-card-body">
-          <a href="${link_data.url}" class="bk-link-card-title headerlink"
-            >${link_data.title}</a
-          >
-          <div class="bk-link-card-subtitle">
-            <div>${link_data.subtitle}</div>
-          </div>
-        </div>
-      </div>
-      <div class="bk-link-card-bg" id="bk-link-card-bg-${link_data.number}" style="background-color: ${link_data.color_rgb};">
-        <div class="bk-link-card-bg-inner"></div>
-      </div>
-    </div>
-  `;
-}
-
-// 在 HTML 中動態生成模板並填入資料
-const link_card_template_Container = document.getElementById(
-  "bk-link-card-template"
-);
-link_card_info.forEach((link_data) => {
-  link_card_template_Container.innerHTML += creatTemplate_link_card(link_data);
-});
